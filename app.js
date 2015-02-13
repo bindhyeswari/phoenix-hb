cb826571d93414f82167c89b9f08b74670e37690
pvtech <tech@golivelabs.io>
2015-02-12 16:39:33 -0800

    asdf

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
 mongoose.connect('mongodb://localhost/test', function (err) {
     if (err) console.log('ERR: Mongoose connect failed!');
--- routes/candidates_route.js
+++ routes/candidates_route.js
@@ -6,7 +6,7 @@ var CandidateModel = require('../models/candidate');
 
 var router = require('express').Router();
 
-var talin = new CandidateModel({
+/*var talin = new CandidateModel({
     name: 'Talin',
     email: 'talin@gmail.com',
     college: 'UCSC',
@@ -16,6 +16,33 @@ var talin = new CandidateModel({
 talin.save(function (err, result) {
     if(err) console.log('There was an error saving ' + talin);
     else console.log(result);
+});*/
+
+router.post('/', function (req, res) {
+    (new CandidateModel(req.body)).save(function(err, result) {
+        // todo: Do not send err information back to the user
+        if (err) res.status(500).json(err);
+        else res.status(201).json(result);
+    });
+});
+
+router.get('/', function (req, res) {
+    if (req.headers.accept.indexOf('application/json') !== -1) {
+        CandidateModel.find(function (err, result) {
+            if (err) res.status(500).json(err);
+            else res.status(200).json(result);
+        });
+    } else {
+        res.render('candidates');
+    }
+
+});
+
+router.get('/:id', function (req, res) {
+    CandidateModel.findById(req.params.id, function (err, result) {
+        if (err) res.status(500).json(err);
+        else res.status(200).json(result);
+    });
 });
 
 module.exports = router;
